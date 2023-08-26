e66f07d179ec20fbcd94437cce36020d65419682
marci <kismarczirobi@gmail.com>
2023-08-26 15:24:33 +0200

    Add dbcontext

---
+++ EnglishHelperService.Persistence/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using EnglishHelperService.Persistence.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EnglishHelperService.Persistence
+{
+	public class DataContext : DbContext
+	{
+		public DataContext(DbContextOptions options) : base(options)
+		{
+		}
+
+		public DbSet<User> Users { get; set; }
+	}
+}
---
+++ EnglishHelperService.Persistence/Entities/User/User.cs
@@ -0,0 +1,12 @@
+﻿namespace EnglishHelperService.Persistence.Entities
+{
+	public class User
+	{
+		public int Id { get; set; }
+		public string Username { get; set; }
+		public string Email { get; set; }
+		public byte[] PasswordHash { get; set; }
+		public byte[] PasswordSalt { get; set; }
+	}
+}
+
